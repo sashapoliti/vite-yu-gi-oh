dcd0edeeb4002eae92c4ff0470e57e871688a450
sashapoliti <sas.politi@gmail.com>
2024-04-15 17:18:23 +0200

    header component style done

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,6 +1,7 @@
 <template>
-    <header>
-        ciao
+    <header class="d-flex align-items-center ">
+        <img src="/images/logo.png" alt="Logo">
+        <h1>Yu-Gi-Oh Api</h1>
     </header>
 </template>
 
@@ -11,5 +12,15 @@
 </script>
 
 <style lang="scss" scoped>
-
+@use '../assets/styles/partials/variables' as *;
+    header {
+        width: 100%;
+        background-color: $thirdcolor;
+        color: $secondarycolor;
+        padding: 20px;
+        img {
+            height: 70px;
+            margin-right: 20px;
+        }
+    }
 </style>
